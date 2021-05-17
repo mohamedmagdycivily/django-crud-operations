21c5c9957bebb8255a38a2cd79e4f7ff6123a58f
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-05-18 01:05:53 +0200

    first commit

---
+++ opensource/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from opensource.models import Student, Track
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+        widgets={
+            "fname":forms.TextInput(attrs={"class":'form-control'}),
+            "lname":forms.TextInput(attrs={"class":'form-control'}),
+            "age":forms.NumberInput(attrs={"class":'form-control'}),
+            "student_track":forms.Select(attrs={"class":'form-control'})
+        }
+
+class TrackForm(forms.ModelForm):
+    class Meta:
+        model = Track
+        fields = ('name', )
